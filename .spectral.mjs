5287ff32730f4dbb4bca6aee8131770cab867a84
unstoppable-mango <ecr.referee@hotmail.com>
2023-03-26 23:34:42 +0000

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjgyMzg3/spectral.js?branch_slug=stoplight&token=c90b5e44-e55a-4a03-b757-2385d1de3ac2";
+export default { extends: ruleset };
